561289e34ab90622566b566a09c9c94288aa835f
tanjimahsankayes@gmail.com <tanjimahsankayes@gmail.com>
2026-07-17 18:37:33 +0600

    deployment signup issue check

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,15 +1,9 @@
-// import { jwtClient } from "better-auth/client/plugins";
-// import { createAuthClient } from "better-auth/react";
-
-// export const authClient = createAuthClient({
-//   baseURL: process.env.NEXT_PUBLIC_APP_URL,
-//   plugins: [jwtClient()],
-// });
-
-// export const { signIn, signUp, signOut, useSession } = createAuthClient();
+throw new Error("AUTH CLIENT LOADED");
 
 import { jwtClient } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
+
+console.log("AUTH CLIENT BASE URL:", process.env.NEXT_PUBLIC_APP_URL);
 
 export const authClient = createAuthClient({
   baseURL: process.env.NEXT_PUBLIC_APP_URL,
